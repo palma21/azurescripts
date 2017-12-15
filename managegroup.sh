96c11ccc75e5d0aa11dd014a744eeb14178234d1
Jorge Palma <jpalma@microsoft.com>
2017-12-15 01:59:27 +0000

    Added managevm and managegroup scripts

---
+++ managegroup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+AZURE_RESOURCE_GROUP=$2
+
+if [ $1 == "delete" ];
+then
+    if [ $AZURE_RESOURCE_GROUP == "all" ];
+    then
+        az group delete --name $(az group list --query "[].name" -o tsv)
+    else
+        GROUP_NAMES="${@:2}"
+        for NAME in $GROUP_NAMES
+        do
+            echo "Deleting $NAME"
+            az group delete --name $NAME --no-wait
+        done
+    fi
+else
+    echo "no valid action"
+fi
+
---
+++ managevm.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+AZURE_RESOURCE_GROUP=$2
+
+if [ $1 == "start" ];
+then
+    if [ $AZURE_RESOURCE_GROUP == "all" ];
+    then
+        az vm start --ids $(az vm list --query "[].id" -o tsv)
+    else
+        VM_NAMES=$(az vm list -g $AZURE_RESOURCE_GROUP --show-details --query "[?powerState=='VM deallocated'].{ name: name }" -o tsv)
+        for NAME in $VM_NAMES
+        do
+            echo "Starting $NAME"
+            az vm start -n $NAME -g $AZURE_RESOURCE_GROUP --no-wait
+        done
+    fi
+fi
+
+if [ $1 == "stop" ];
+then
+    if [ $AZURE_RESOURCE_GROUP == "all" ];
+    then
+        az vm deallocate --ids $(az vm list --query "[].id" -o tsv)
+    else
+        VM_NAMES=$(az vm list -g $AZURE_RESOURCE_GROUP --show-details --query "[?powerState=='VM running'].{ name: name }" -o tsv)
+        for NAME in $VM_NAMES
+        do
+            echo "Stopping $NAME"
+            az vm deallocate -n $NAME -g $AZURE_RESOURCE_GROUP --no-wait
+        done
+    fi
+fi
+
+if [ $1 == "status" ];
+then
+    echo "Fetching Details..."
+    if [ $AZURE_RESOURCE_GROUP == "all" ];
+    then
+        az vm list --show-details
+    else
+        az vm list -g $AZURE_RESOURCE_GROUP --show-details
+    fi
+    echo "Fetching Summary..."
+    echo "Power Status of all VMs"
+    echo "-----------------------"
+    if [ $AZURE_RESOURCE_GROUP == "all" ];
+    then
+        az vm list --show-details --query "[].{name: name, status: powerState}" -o table
+    else
+        az vm list -g $AZURE_RESOURCE_GROUP --show-details --query "[].{name: name, status: powerState}" -o table
+    fi
+fi
+
+
+if [ $1 == "ip" ];
+then
+    az vm list -g $AZURE_RESOURCE_GROUP --show-details --query "[?powerState=='VM running'].{ name:name, ip: publicIps }" -o table
+fi
